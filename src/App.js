517d9064a11d04f08a64d59cda8513c9c2d84676
Ashesh SINHA <tech.asinha@gmail.com>
2024-03-18 14:50:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Dropdowns from './dropDowns/dropdowns';
+function App() {
+  return (
+    <div className="App">
+      <Dropdowns />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dropDowns/dropdowns.jsx
@@ -0,0 +1,95 @@
+import axios from 'axios'
+import React from 'react'
+import styles from "./dropdowns.module.css"
+import { useEffect , useState } from 'react'
+const Dropdowns = () => {
+  const [countries , setCountries] = useState([])
+  const [states , setStates] = useState([])
+  const [cities , setCities] = useState([])
+  const [selectedCountry,setSelectedCountry] = useState("")
+  const [selectedState,setSelectedState] = useState("")
+  const [selectedCity, setSelectedCity] = useState("")
+  useEffect(()=>{
+    (async function(){
+        try {
+          let res = await axios.get("https://crio-location-selector.onrender.com/countries")
+          console.log(res.data)
+          setCountries(res.data)
+        } catch (error) {
+          console.log(error)
+        }
+    })
+    ()
+  },[])
+  // console.log(selectedCountry)
+  useEffect(()=>{
+    (async function(){
+      if (selectedCountry)
+      try {
+         let res = await axios.get(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
+         setStates(res.data)
+         setSelectedState("")
+         setCities([])
+         setSelectedCity("")
+        } catch (error) {
+         console.log(error)
+      }
+    })
+    ()
+  },[selectedCountry])
+   
+   useEffect(()=>{
+    (async function () {
+      if (selectedCountry && selectedState) {
+        try {
+          let data = await axios.get(
+            `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+          );
+          setCities(data.data);
+          setSelectedCity("");
+          //  console.log(data.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    })();
+  }, [selectedCountry, selectedState]);
+   
+  return (
+    <div>
+      <h1>Select Location</h1>
+      <div className={styles.dropDowns}>
+        <select className={styles.dropDown} value={selectedCountry} onChange={(e)=> setSelectedCountry(e.target.value)}>
+          <option value="">select country</option>
+          {countries.map((country)=>(
+            <option value={country} key={country}>{country}</option> 
+          ))}
+        </select>
+        <select className={styles.dropDown} value={selectedState} onChange={(e)=> setSelectedState(e.target.value)}>
+          <option value="">select country</option>
+          {states.map((state)=>(
+            <option value={state} key={state}>{state}</option> 
+          ))}
+        </select>
+        <select className={styles.dropDown} value={selectedCity} onChange={(e)=> setSelectedCity(e.target.value)}>
+          <option value="">select country</option>
+          {cities.map((city)=>(
+            <option value={city} key={city}>{city}</option> 
+          ))}
+        </select>
+      </div>
+      {selectedCity && selectedState && selectedCountry && (
+        <h3>
+          <span className={styles.first}> You selected </span>
+          <span className={styles.big}>{selectedCity},</span>
+          <span className={styles.fade}>
+            {" "}
+            {selectedState}, {selectedCountry}
+          </span>
+        </h3>
+      )}
+    </div>
+  )
+}
+
+export default Dropdowns
